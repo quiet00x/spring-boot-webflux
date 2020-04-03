5f2d1e397453cec5e50b130783f34b028bace29f
quiet <quiet_inside@126.com>
2020-04-04 00:57:45 +0800

    增加Mybatis-plus分页插件

---
+++ src/main/java/com/example/webflux/service/UserService.java
@@ -0,0 +1,11 @@
+package com.example.webflux.service;
+
+import com.baomidou.mybatisplus.extension.service.IService;
+import com.example.webflux.domain.User;
+
+/**
+ * @project_name: spring-boot-webflux
+ * @date: 2020/4/3 - 21:43
+ * @author: Mr_Bangb
+ */
+public interface UserService extends IService<User> { }
