0b458540da2a41387d0800cae9568b4e149c09ce
quiet <quiet_inside@126.com>
2020-04-03 21:06:09 +0800

    修改异常处理器错误

--- src/main/java/com/example/webflux/common/exception/GlobalExceptionHandler.java
+++ src/main/java/com/example/webflux/common/exception/GlobalExceptionHandler.java
@@ -9,7 +9,6 @@ import org.springframework.validation.BindingResult;
 import org.springframework.validation.FieldError;
 import org.springframework.validation.ObjectError;
 import org.springframework.web.bind.MethodArgumentNotValidException;
-import org.springframework.web.bind.MissingServletRequestParameterException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
@@ -25,41 +24,38 @@ import java.util.List;
 @Slf4j
 public class GlobalExceptionHandler {
 
-    public class GlobalExceptionHandler {
+    /**
+     * 缺少请求体异常处理器
+     * @param e 缺少请求体异常
+     * @return ResponseResult
+     */
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    @ExceptionHandler(HttpMessageNotReadableException.class)
+    public ResponseResult parameterBodyMissingExceptionHandler(HttpMessageNotReadableException e) {
+        log.error("", e);
+        return new ResponseResult(ResultEnum.PARAMETER_ERROR.getCode(), "参数体不能为空");
+    }
 
-        /**
-         * 缺少请求体异常处理器
-         * @param e 缺少请求体异常
-         * @return ResponseResult
-         */
-        @ResponseStatus(HttpStatus.BAD_REQUEST)
-        @ExceptionHandler(HttpMessageNotReadableException.class)
-        public ResponseResult parameterBodyMissingExceptionHandler(HttpMessageNotReadableException e) {
-            log.error("", e);
-            return new ResponseResult(ResultEnum.PARAMETER_ERROR.getCode(), "参数体不能为空");
-        }
-
-        /**
-         * 参数效验异常处理器
-         * @param e 参数验证异常
-         * @return ResponseInfo
-         */
-        @ResponseStatus(HttpStatus.BAD_REQUEST)
-        @ExceptionHandler(MethodArgumentNotValidException.class)
-        public ResponseResult parameterExceptionHandler(MethodArgumentNotValidException e) {
-            log.error("", e);
-            // 获取异常信息
-            BindingResult exceptions = e.getBindingResult();
-            // 判断异常中是否有错误信息，如果存在就使用异常中的消息，否则使用默认消息
-            if (exceptions.hasErrors()) {
-                List<ObjectError> errors = exceptions.getAllErrors();
-                if (!errors.isEmpty()) {
-                    // 这里列出了全部错误参数，按正常逻辑，只需要第一条错误即可
-                    FieldError fieldError = (FieldError) errors.get(0);
-                    return new ResponseResult(ResultEnum.PARAMETER_ERROR.getCode(), fieldError.getDefaultMessage());
-                }
+    /**
+     * 参数效验异常处理器
+     * @param e 参数验证异常
+     * @return ResponseInfo
+     */
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseResult parameterExceptionHandler(MethodArgumentNotValidException e) {
+        log.error("", e);
+        // 获取异常信息
+        BindingResult exceptions = e.getBindingResult();
+        // 判断异常中是否有错误信息，如果存在就使用异常中的消息，否则使用默认消息
+        if (exceptions.hasErrors()) {
+            List<ObjectError> errors = exceptions.getAllErrors();
+            if (!errors.isEmpty()) {
+                // 这里列出了全部错误参数，按正常逻辑，只需要第一条错误即可
+                FieldError fieldError = (FieldError) errors.get(0);
+                return new ResponseResult(ResultEnum.PARAMETER_ERROR.getCode(), fieldError.getDefaultMessage());
             }
-            return new ResponseResult(ResultEnum.PARAMETER_ERROR);
         }
+        return new ResponseResult(ResultEnum.PARAMETER_ERROR);
     }
 }
