9ea08c6de00af0ce4e568f874bc10525e61d1338
quiet <quiet_inside@126.com>
2020-04-06 01:14:42 +0800

    修改注释

--- src/main/java/com/example/webflux/config/properties/DictionaryPropertiesConfiguration.java
+++ src/main/java/com/example/webflux/config/properties/DictionaryPropertiesConfiguration.java
@@ -10,14 +10,14 @@ import org.springframework.context.annotation.PropertySource;
  * @project_name: spring-boot-webflux
  * @date: 2020/4/5 - 3:47
  * @author: Mr_Bangb
- * 数据字典，存放常用的共有属性
+ * 加载自定义properties文件至JavaBean对象中
  * 1.利用@PropertySource("classpath:dictionary.properties") 加载自定义properties文件，
  * 2.并将属性注入到DictionaryBean中
- * 问题是：
+ * 问题：
  *  1. 该种方式加载的properties文件，必须制定注入至某个JavaBean中
  *  2. 增加一个数据字典项，必须增加对应的JavaBean属性
  *  3. 而且一配置文件只能加载一个properties文件，对应一个JavaBean
- *  不太建议使用
+ *  不太建议使用！
  */
 @Configuration
 @PropertySource("classpath:dictionary.properties")
